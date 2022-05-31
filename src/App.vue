3f6005b1b6f773ea351ceaab54468600373e7a22
Pooya Golchian <pooya.golchian@gmail.com>
2022-06-01 00:16:52 +0400

    add gsap animation

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="container" id="app">
     <NavCmp></NavCmp>
+    <CustomeCursor></CustomeCursor>
+
     <router-view />
   </div>
 </template>
 
 <script>
 import NavCmp from "@/components/nav-cmp";
+import CustomeCursor from "./components/custome-cursor.vue";
 
 export default {
   components: {
     NavCmp,
+    CustomeCursor,
   },
 };
 </script>
